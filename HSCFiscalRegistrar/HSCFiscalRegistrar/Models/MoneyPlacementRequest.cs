698b3e4a03669c335417a79f8d135b871e109975
Ramazan Mamedov <mamedov.r9657@gmail.com>
2019-08-09 13:51:11 +0600

    #4 Добавил модель MoneyPlacementRequest, определил его поля. Добавил MoneyPlacementEnum с двумя полями, MoneyPlacementDeposit = 0,  MoneyPlacementWithdrawal = 1

---
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Models/MoneyPlacementRequest.cs
@@ -0,0 +1,14 @@
+using System;
+using HSCFiscalRegistrar.Enums;
+
+namespace HSCFiscalRegistrar.Models
+{
+    public class MoneyPlacementRequest
+    {
+        public DateTime DateTime { get; set; }
+        public MoneyPlacementEnum Operation { get; set; }
+        public Money Sum { get; set; }
+        public bool IsOffline { get; set; }
+        public int FrShiftNumber { get; set; }
+    }
+}
