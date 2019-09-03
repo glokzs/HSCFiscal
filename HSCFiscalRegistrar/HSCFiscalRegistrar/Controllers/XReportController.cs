3c832d5b8d33e1e37ff2e5623cd0cd3e845b1440
Nursu <myliferap93>
2019-09-03 21:00:57 +0600

    Добавил атрибут

--- HSCFiscalRegistrar/HSCFiscalRegistrar/Controllers/XReportController.cs
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Controllers/XReportController.cs
@@ -6,6 +6,7 @@ namespace HSCFiscalRegistrar.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class XReportController : Controller
     {
         [HttpPost]
