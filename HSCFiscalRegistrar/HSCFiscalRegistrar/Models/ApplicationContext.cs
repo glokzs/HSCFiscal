17eb6ed6f68a75d74e6ae882a95bc6487570d833
Ramazan Mamedov <mamedov.r9657@gmail.com>
2019-09-06 14:39:12 +0600

    #30 Добавил в ApplicationContext поля для добавления в БД. Добавил LazyLoad

--- HSCFiscalRegistrar/HSCFiscalRegistrar/Models/ApplicationContext.cs
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Models/ApplicationContext.cs
@@ -1,14 +1,24 @@
+using HSCFiscalRegistrar.Models.APKInfo;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HSCFiscalRegistrar.Models
 {
-    public class ApplicationContext : IdentityDbContext<User>
+    public sealed class ApplicationContext : IdentityDbContext<User>
     {
+        public DbSet<Kkm> Kkms { get; set; }
+        public DbSet<Org> Orgs { get; set; }
+        public DbSet<RegInfo> RegInfos { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Request> Requests { get; set; }
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
             Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
         }
     }
 }
