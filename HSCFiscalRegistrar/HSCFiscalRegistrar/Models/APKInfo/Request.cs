a925d3ceeae6c3e9aad1b80c9692d2ad2e574398
Ramazan Mamedov <mamedov.r9657@gmail.com>
2019-09-06 15:09:41 +0600

    #30 Добавил модификатор virtual для полей объектов. Создал миграцию AddedInfo

--- HSCFiscalRegistrar/HSCFiscalRegistrar/Models/APKInfo/Request.cs
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Models/APKInfo/Request.cs
@@ -2,11 +2,12 @@ namespace HSCFiscalRegistrar.Models.APKInfo
 {
     public class Request
     {
+        public string Id { get; set; }
         public string Command { get; set; }
         public int DeviceId { get; set; }
         public int ReqNum { get; set; }
         public int Token { get; set; }
         public string ServiceId { get; set; }
-        public Service Service { get; set; }
+        public virtual Service Service { get; set; }
     }
 }
