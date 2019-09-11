66d9ad82c24e9aa3a45aa146d8d34ebb78d57eda
Ramazan Mamedov <mamedov.r9657@gmail.com>
2019-09-11 13:52:39 +0600

    #34 реализовал отправку единиц измерения с помощью считывания Json файла и отправки Json объекта

---
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Directories/Unit.cs
@@ -0,0 +1,7 @@
+namespace HSCFiscalRegistrar.Directories
+{
+    public class Unit
+    {
+        public UnitData[] Data { get; set; }
+    }
+}
---
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Directories/UnitData.cs
@@ -0,0 +1,12 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace HSCFiscalRegistrar.Directories
+{
+    public class UnitData
+    {
+        public int Code { get; set; }
+        public string NameRu { get; set; }
+        public string NameEn { get; set; }
+        public string NameKz { get; set; }
+    }
+}
