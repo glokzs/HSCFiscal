3285590e6ca945a4ba84b898b0412f6fe52e86d4
Ibragim Yunussov <ibragim.yu92@gmail.com>
2019-09-20 16:46:21 +0600

    Создал модель смены

---
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Models/Shift.cs
@@ -0,0 +1,17 @@
+﻿using HSCFiscalRegistrar.DTO.DateAndTime;
+
+namespace HSCFiscalRegistrar.Models
+{
+    public class Shift
+    {
+        public string Id { get; set; }
+        public int Number { get; set; }
+        public decimal TotalSum { get; set; }
+        public int TotalCount { get; set; }
+        public DateTime OpenDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public int TerminalReqNum { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
