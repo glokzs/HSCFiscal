e190aed2dda50c40bae19b631b69ee083a648534
Vitaliy <pasko.vitaliy24@gmail.com>
2019-10-04 20:29:43 +0600

    #53 Создал класс для генерации фискального номера.

---
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Helpers/GenerateFiscalSign.cs
@@ -0,0 +1,14 @@
+using System;
+namespace HSCFiscalRegistrar.Helpers
+{
+    public class GenerateFiscalSign
+    
+    {
+        public static int FiscalSign { get; set; }
+        
+        public static int GeneratorFiscalSign()
+        {
+            
+        }
+    }
+}
