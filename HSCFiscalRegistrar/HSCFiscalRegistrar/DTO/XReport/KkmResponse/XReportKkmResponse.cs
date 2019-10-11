96858eaed481614127daaf2689a449f8f4b99a23
Ramazan Mamedov <mamedov.r9657@gmail.com>
2019-10-11 16:37:02 +0600

    #57 Рефактор кода, создал конструктор для создания объекта XReportKkmResponse чтобы вынести из контроллера заполнение полей класса

--- HSCFiscalRegistrar/HSCFiscalRegistrar/DTO/XReport/KkmResponse/XReportKkmResponse.cs
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/DTO/XReport/KkmResponse/XReportKkmResponse.cs
@@ -1,7 +1,79 @@
-﻿namespace HSCFiscalRegistrar.DTO.XReport.KkmResponce
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HSCFiscalRegistrar.Enums;
+using HSCFiscalRegistrar.Models;
+using HSCFiscalRegistrar.Models.APKInfo;
+using HSCFiscalRegistrar.Models.Operation;
+
+namespace HSCFiscalRegistrar.DTO.XReport.KkmResponce
 {
     public class XReportKkmResponse
     {
         public Data Data { get; set; }
+
+        public XReportKkmResponse(List<ShiftOperation> shiftOperations,
+            IQueryable<Operation> operations,
+            Org org, Kkm kkm, Shift shift, Operator oper)
+        {
+            Data = new Data
+            {
+                Buy = GetOperation(shiftOperations, OperationTypeEnum.OPERATION_BUY),
+                Sell = GetOperation(shiftOperations, OperationTypeEnum.OPERATION_SELL),
+                ReturnBuy = GetOperation(shiftOperations, OperationTypeEnum.OPERATION_BUY_RETURN),
+                ReturnSell = GetOperation(shiftOperations, OperationTypeEnum.OPERATION_SELL_RETURN),
+                TaxPayerName = org.Name,
+                TaxPayerVAT = org.VAT,
+                TaxPayerIN = org.Inn,
+                TaxPayerVATNumber = org.VATNumber,
+                TaxPayerVATSeria = org.VATSeria,
+                CashboxIN = kkm.DeviceId,
+                CashboxSN = kkm.SerialNumber,
+                CashboxRN = kkm.FnsKkmId,
+                StartOn = shift.OpenDate,
+                CloseOn = shift.CloseDate,
+                ReportOn = DateTime.Now,
+                CashierCode = oper.Code,
+                ShiftNumber = shift.Number,
+                ControlSum = 1,
+                DocumentCount = operations.Count(),
+                OfflineMode = false,
+                ReportNumber = 1,
+                CashboxOfflineMode = false,
+                EndNonNullable = new NonNullableApiModel()
+                {
+                    Sell = shift.SellSaldoEnd,
+                    Buy = shift.BuySaldoEnd,
+                    ReturnBuy = shift.RetunBuySaldoEnd,
+                    ReturnSell = shift.RetunSellSaldoEnd
+                },
+                StartNonNullable = new NonNullableApiModel()
+                {
+                    Sell = shift.SellSaldoBegin,
+                    Buy = shift.BuySaldoBegin,
+                    ReturnBuy = shift.RetunBuySaldoBegin,
+                    ReturnSell = shift.RetunSellSaldoBegin
+                },
+                SumInCashbox = shift.KkmBalance,
+                PutMoneySum = 0,
+                TakeMoneySum = 0
+            };
+        }
+        private OperationTypeSummaryApiModel GetOperation(List<ShiftOperation> shiftOperations, OperationTypeEnum type)
+        {
+            return new OperationTypeSummaryApiModel
+            {
+                Change = 0,
+                Taken = shiftOperations.Where(o => o.OperationType == type)
+                    .Sum(o => o.TotalAmount),
+                VAT = 0,
+                TotalCount = shiftOperations.Where(o => o.OperationType == type)
+                    .Sum(o => o.Count),
+                PaymentsByTypesApiModel = new List<PaymentsByTypesApiModel>(),
+                Discount = 0,
+                Markup = 0,
+                Count = 0
+            };
+        }
     }
 }
