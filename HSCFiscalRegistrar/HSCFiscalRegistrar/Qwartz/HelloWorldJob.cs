1f507af2b97a35c8e28568fa8d13ac59cf9755f8
Nursultan Orynbayev <nursultan.orynbayev@gmail.com>
2019-10-14 17:37:09 +0600

    #64 добавил Job

---
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Qwartz/HelloWorldJob.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System.Threading.Tasks;
+
+
+namespace HSCFiscalRegistrar.Qwartz
+{
+    [DisallowConcurrentExecution]
+    public class HelloWorldJob : IJob
+    {
+        private readonly ILogger<HelloWorldJob> _logger;
+        
+        public HelloWorldJob(ILogger<HelloWorldJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello world!");
+            return Task.CompletedTask;
+        }
+        
+    }
+}
