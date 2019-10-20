d9c8a6064243d3f2455e50f73517f729dd48d23f
Ibragim Yunussov <ibragim.yu92@gmail.com>
2019-10-20 06:52:31 +0600

    Добавил валидацию на имя кассы

--- HSCFiscalRegistrar/Fiscal/ViewModels/RegisterCashDesckViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fiscal.ViewModels
-{
-    public class RegisterCashDesckViewModel
-    {
-        public string CashDesckName { get; set; }
-        public string Description { get; set; }
-        public string UserId { get; set; }
-    }
-}
---
+++ HSCFiscalRegistrar/Fiscal/ViewModels/RegisterCashDeskViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiscal.ViewModels
+{
+    public class RegisterCashDeskViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        [Remote("CheckName", "Initialization", ErrorMessage = "Это имя уже занято", AdditionalFields = "Id")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+    }
+}
