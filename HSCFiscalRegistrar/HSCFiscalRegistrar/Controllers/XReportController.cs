addeb8ca7295f862dd861bf6e4304ec41502f7a5
Ramazan Mamedov <mamedov.r9657@gmail.com>
2019-10-23 21:25:37 +0600

    Исправил логирование в XReportController

--- HSCFiscalRegistrar/HSCFiscalRegistrar/Controllers/XReportController.cs
+++ HSCFiscalRegistrar/HSCFiscalRegistrar/Controllers/XReportController.cs
@@ -6,7 +6,6 @@ using HSCFiscalRegistrar.OfdRequests;
 using HSCFiscalRegistrar.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Models;
 using Models.DTO.XReport;
 using Models.DTO.XReport.KkmResponse;
@@ -21,25 +20,20 @@ namespace HSCFiscalRegistrar.Controllers
     {
         private readonly ApplicationContext _applicationContext;
         private readonly UserManager<User> _userManager;
-        private readonly ILoggerFactory _loggerFactory;
         private readonly GenerateErrorHelper _errorHelper;
 
-        public XReportController(ApplicationContext applicationContext, UserManager<User> userManager,
-            ILoggerFactory loggerFactory, GenerateErrorHelper errorHelper)
+        public XReportController(ApplicationContext applicationContext, UserManager<User> userManager, GenerateErrorHelper errorHelper)
         {
             _applicationContext = applicationContext;
             _userManager = userManager;
-            _loggerFactory = loggerFactory;
             _errorHelper = errorHelper;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] KkmRequest request)
         {
-            var logger = _loggerFactory.CreateLogger("XReport|Post");
             try
             {
-                logger.LogInformation($"X-Отчет: {request.Token}");
                 var user = _userManager.Users.FirstOrDefault(u => u.UserToken == request.Token);
                 var kkm = _applicationContext.Kkms.FirstOrDefault(k => k.Id == user.KkmId);
                 var merch = _userManager.Users.FirstOrDefault(u => user.OwnerId == u.Id);
@@ -52,7 +46,6 @@ namespace HSCFiscalRegistrar.Controllers
                 {
                     shift = await GetShift(user,kkm);
                 }
-                
                 var operations = _applicationContext.Operations.Where(o => o.ShiftId == shift.Id);
                 var shiftOperations = ZxReportService.GetShiftOperations(operations, shift);
                 ZxReportService.AddShiftProps(shift, operations);
@@ -61,14 +54,13 @@ namespace HSCFiscalRegistrar.Controllers
                 kkm.ReqNum += 1;
                 await _applicationContext.ShiftOperations.AddRangeAsync(shiftOperations);
                 await _applicationContext.SaveChangesAsync();
-                var xReportOfdRequest = new OfdXReport(_loggerFactory);
+                var xReportOfdRequest = new OfdXReport();
                 xReportOfdRequest.Request(kkm, merch);
 
                 return Ok(JsonConvert.SerializeObject(response));
             }
             catch (Exception e)
             {
-                logger.LogError(e.Message);
                 return Json(e.Message);
             }
         }
